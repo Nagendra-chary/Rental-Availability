f341683ad6ad9bfaea2eaf8547e92d91db579755
sairam.rugada <sairam.rugada>
2017-02-24 19:40:36 +0530

    @Added sidebar and footer.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
 /*Added by Sairam Rugada*/
+$(document).ready(function(){
+	var window_height = window.innerHeight;
+	var footer_height = Number($('footer').css('height').split('px')[0]); //67
+	var sidebar_height = window_height - footer_height;
+	$('.sidebar').css('height', sidebar_height);
+});
